b0f59a6208b7fd12f4b0a180a7fd7ccad8ce39ea
anjali <anjali.thorat1510@gmail.com>
2024-10-30 11:48:28 +0530

    feat: adding langing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Header from './components/Header'
-import Category from './components/Category'
 import LandingPage from './components/LandingPage'
+import Category from './features/category/Category'
 
 const App = () => {
   return (
